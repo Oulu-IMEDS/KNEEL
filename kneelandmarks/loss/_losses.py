f1e65e6f092ba906f449a30520b06b67f5cc4a15
lext <aleksei.tiulpin@protonmail.ch>
2019-06-01 23:11:04 +0300

    Implemented wing loss

--- kneelandmarks/loss/_losses.py
+++ kneelandmarks/loss/_losses.py
@@ -1,6 +1,7 @@
 from torch import nn
 import torch
 from torch.nn import functional as F
+import math
 
 
 class ElasticLoss(nn.Module):
@@ -22,19 +23,62 @@ class ElasticLoss(nn.Module):
         return loss
 
 
-class HGHMLoss(nn.Module):
-    def __init__(self, hm_loss='mse', w=0.5):
-        super(HGHMLoss, self).__init__()
-        assert hm_loss in ['mse', 'elastic']
-
-        if hm_loss == 'mse':
-            self.hm_loss = nn.MSELoss()
+class LNLoss(nn.Module):
+    def __init__(self, space='l2'):
+        super(LNLoss, self).__init__()
+        if space == 'l2':
+            self.loss = F.mse_loss
+        elif space == 'l1':
+            self.loss = F.l1_loss
         else:
-            self.hm_loss = ElasticLoss()
+            raise NotImplementedError
 
-        self.weights = torch.FloatTensor([w, 1-w])
+    def forward(self, preds, gt):
+        loss = 0
 
-    def forward(self, preds, gt_hm):
-        pred_hm_0, pred_hm_1 = preds
-        return self.hm_loss(pred_hm_0, gt_hm).mul(self.weights[0]) + self.hm_loss(pred_hm_1, gt_hm).mul(self.weights[1])
+        if not isinstance(preds, tuple):
+            preds = (preds, )
+
+        for i in range(len(preds)):
+            loss += self.loss(preds[i].squeeze(), gt.squeeze())
+
+        return loss
+
+
+class WingLoss(nn.Module):
+    """
+    https://arxiv.org/pdf/1711.06753.pdf
+
+    Refactored implementation from  https://github.com/BloodAxe/pytorch-toolbelt/.
+    Supports the input from stacked hourglass networks.
+
+    """
+    def __init__(self, width=5, curvature=0.5):
+        super(WingLoss, self).__init__()
+        self.w = width
+        self.curvature = curvature
+        self.c = self.w - self.w * math.log(1 + self.w / self.curvature)
+
+    def __forward_single(self,preds, target):
+        diff_abs = (target - preds).abs()
+        loss = diff_abs.clone()
+
+        idx_smaller = diff_abs < self.w
+        idx_bigger = diff_abs >= self.w
+
+        loss[idx_smaller] = self.w * torch.log(1 + diff_abs[idx_smaller] / self.curvature)
+        loss[idx_bigger] = loss[idx_bigger] - self.c
+
+        return loss.mean()
+
+    def forward(self, preds, target):
+        loss = 0
+
+        if not isinstance(preds, tuple):
+            preds = (preds, )
+
+        for i in range(len(preds)):
+            loss += self.__forward_single(preds[i], target)
+
+        return loss
 
--- kneelandmarks/loss/_utils.py
+++ kneelandmarks/loss/_utils.py
@@ -1,11 +1,18 @@
 from deeppipeline.kvs import GlobalKVS
-from ._losses import HGHMLoss, ElasticLoss
+from ._losses import LNLoss, ElasticLoss, WingLoss
 
 
 def init_loss():
     kvs = GlobalKVS()
-    if kvs['args'].sagm:
+    if kvs['args'].loss_type:
         loss = ElasticLoss(w=kvs['args'].loss_weight)
+    elif kvs['args'].loss_type == 'l2':
+        loss = LNLoss(space='l2')
+    elif kvs['args'].loss_type == 'l1':
+        loss = LNLoss(space='l1')
+    elif kvs['args'].loss_type == 'wing':
+        loss = WingLoss(width=kvs['args'].wing_w, curvature=kvs['args'].wing_c)
     else:
-        loss = HGHMLoss(hm_loss=kvs['args'].hm_loss, w=kvs['args'].loss_weight)
+        raise NotImplementedError
+
     return loss.to('cuda')
