56bfc5efcb0c9beee732c39782d6275b8f117e30
lext <aleksei.tiulpin@protonmail.ch>
2019-06-25 19:15:33 +0300

    Integrated generalized robust loss

--- kneelandmarks/loss/_utils.py
+++ kneelandmarks/loss/_utils.py
@@ -1,10 +1,10 @@
 from deeppipeline.kvs import GlobalKVS
-from deeppipeline.common.losses import LNLoss, ElasticLoss, WingLoss
+from deeppipeline.common.losses import LNLoss, ElasticLoss, WingLoss, GeneralizedRobustLoss
 
 
 def init_loss():
     kvs = GlobalKVS()
-    if kvs['args'].loss_type:
+    if kvs['args'].loss_type == 'elastic':
         loss = ElasticLoss(w=kvs['args'].loss_weight)
     elif kvs['args'].loss_type == 'l2':
         loss = LNLoss(space='l2')
@@ -12,6 +12,12 @@ def init_loss():
         loss = LNLoss(space='l1')
     elif kvs['args'].loss_type == 'wing':
         loss = WingLoss(width=kvs['args'].wing_w, curvature=kvs['args'].wing_c)
+    elif kvs['args'].loss_type == 'robust':
+        loss = GeneralizedRobustLoss(num_dims=20 * 2 if kvs['args'].annotations == 'hc' else 2,
+                                     alpha_init=kvs['args'].alpha_robust,
+                                     scale_init=kvs['args'].c_robust,
+                                     alpha_lo=kvs['args'].alpha_robust_min,
+                                     alpha_hi=kvs['args'].alpha_robust_max)
     else:
         raise NotImplementedError
 
--- kneelandmarks/training/_utils.py
+++ kneelandmarks/training/_utils.py
@@ -31,7 +31,7 @@ def pass_epoch(net, loader, optimizer, criterion):
             if not kvs['args'].sagm:
                 target = entry['target_hm'].to(device).squeeze().unsqueeze(1)
             else:
-                target = entry['kp_gt'].to(device)
+                target = entry['kp_gt'].to(device).float()
 
             outputs = net(inputs)
             loss = criterion(outputs, target)
@@ -109,7 +109,7 @@ def pass_epoch(net, loader, optimizer, criterion):
 
 def val_results_callback(writer, val_metrics, to_log, val_results):
     results = []
-    precision = [1, 1.5, 2, 2.5, 3, 5, 10, 15]
+    precision = [1, 1.5, 2, 2.5, 3, 5]
     for kp_id in val_results:
         kp_res = val_results[kp_id]
 
