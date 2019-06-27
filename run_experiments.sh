f1f6360a920e7e7ecb14a1242e8a0191ac2328f7
lext <aleksei.tiulpin@protonmail.ch>
2019-06-27 21:35:19 +0300

    Experiment runner added

---
+++ run_experiments.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+HC_DATA_ROOT=/media/lext/FAST/knee_landmarks/workdir/high_cost_data
+WORKDIR=/media/lext/FAST/knee_landmarks/workdir
+
+for EXP_FILE in $(ls experiments/)
+do
+    python scripts/train.py --dataset_root ${HC_DATA_ROOT} --workdir ${WORKDIR} --experiment experiments/${EXP_FILE}
+done
