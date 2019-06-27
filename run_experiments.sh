ababd0d499efc81425eda8bdafa832678387d889
lext <aleksei.tiulpin@protonmail.ch>
2019-06-27 21:36:28 +0300

    fixed name

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,7 +3,7 @@
 HC_DATA_ROOT=/media/lext/FAST/knee_landmarks/workdir/high_cost_data
 WORKDIR=/media/lext/FAST/knee_landmarks/workdir
 
-for EXP_FILE in $(ls experiments/)
+for EXP_FILE in $(ls hc_experiments/)
 do
     python scripts/train.py --dataset_root ${HC_DATA_ROOT} --workdir ${WORKDIR} --experiment experiments/${EXP_FILE}
 done
