7e9d05f27e319955f3417a3d14bdd89ff1021b13
lext <aleksei.tiulpin@protonmail.ch>
2019-07-04 23:44:47 +0300

    Bash script refactor

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,11 +4,6 @@ HC_DATA_ROOT=/media/lext/FAST/knee_landmarks/workdir/high_cost_data
 WORKDIR=/media/lext/FAST/knee_landmarks/workdir
 EXP_DIR=hc_experiments_todo/
 
-#for EXP_FILE in $(ls ${EXP_DIR})
-#do
-#    python scripts/train.py --dataset_root ${HC_DATA_ROOT} --workdir ${WORKDIR} --experiment ${EXP_DIR}/${EXP_FILE}
-#done
-
 python scripts/experiments_runner.py --data_root ${HC_DATA_ROOT} \
                                      --workdir ${WORKDIR} \
                                      --experiment ${EXP_DIR} \
