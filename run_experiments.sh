e6cdeaa324eb2fbf40988fd02deceb10c14f732c
lext <aleksei.tiulpin@protonmail.ch>
2019-07-12 18:13:47 +0300

    added low cost exp to the exp runner

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-HC_DATA_ROOT=/media/lext/FAST/knee_landmarks/workdir/high_cost_data
 WORKDIR=/media/lext/FAST/knee_landmarks/workdir
 EXP_DIR=hc_experiments_todo/
 
-python scripts/experiments_runner.py --data_root ${HC_DATA_ROOT} \
+python scripts/experiments_runner.py --data_root ${WORKDIR}/high_cost_data \
                                      --workdir ${WORKDIR} \
                                      --experiment ${EXP_DIR} \
                                      --log_dir ${WORKDIR}/experiment_runs_queed \
                                      --script_path scripts/train.py
+
+python scripts/train.py --dataset_root ${WORKDIR}/low_cost_data \
+                        --workdir ${WORKDIR} \
+                        --experiment lc_experiments/low_cost_from_scratch_multiscale_wing_mixup_no_wd_cutout5.yml 
+
 
 for SNP in $(ls ${WORKDIR}/snapshots/ | grep "2019_")
 do
