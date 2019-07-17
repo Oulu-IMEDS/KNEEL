eee7e382b047a5f2e5d0272920e417739ca24f29
lext <aleksei.tiulpin@protonmail.ch>
2019-07-17 16:07:14 +0300

    added some more lines to the make pics script

--- scripts/make_example_pictures.py
+++ scripts/make_example_pictures.py
@@ -1,5 +1,7 @@
 import argparse
 import pandas as pd
+import glob
+import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -10,3 +12,9 @@ if __name__ == '__main__':
     parser.add_argument('--hc_images', default='')
     args = parser.parse_args()
 
+    kls = pd.read_csv(args.kls)
+    lc_annotations = pd.read_csv(args.lc_annotations)
+    hc_annotations = pd.read_csv(args.hc_annotations)
+
+    hc_images = glob.glob(os.path.join(args.hc_images, '*'))
+    lc_images = glob.glob(os.path.join(args.lc_images, '*'))
