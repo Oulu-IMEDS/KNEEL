eda018ab2cb5f02024ce83aee1d5f329a9baa65d
lext <aleksei.tiulpin@protonmail.ch>
2019-10-05 20:51:39 +0300

    inference model loading is parameterized via argparse

--- kneel/inference/app.py
+++ kneel/inference/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import argparse
+from kneel.inference.pipeline import KneeAnnotatorPipeline
 
 app = Flask(__name__)
 
@@ -8,5 +10,19 @@ def analyze_knee():
     data = request.data
     return "Hello World!"
 
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lc_snapshot_path', default='')
+    parser.add_argument('--hc_snapshot_path', default='')
+    parser.add_argument('--roi_size_mm', type=int, default=140)
+    parser.add_argument('--pad', type=int, default=300)
+    parser.add_argument('--device',  default='cuda')
+    parser.add_argument('--refine', type=bool, default=False)
+    parser.add_argument('--mean_std_path', default='')
+    args = parser.parse_args()
+
+    annotator = KneeAnnotatorPipeline(args.lc_snapshot_path, args.hc_snapshot_path,
+                                      args.mean_std_path, args.device)
+
     app.run()
