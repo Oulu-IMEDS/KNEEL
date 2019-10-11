9f401fc30ed78dce7ee0228d12093eefa5292ba0
lext <aleksei.tiulpin@protonmail.ch>
2019-10-11 16:13:30 +0200

    commented docker run in fetch_snapshots

--- fetch_snapshots.sh
+++ fetch_snapshots.sh
@@ -1,16 +1,16 @@
 wget http://mipt-ml.oulu.fi/models/KNEEL/snapshots_release.tar.xz
 tar -xvf snapshots_release.tar.xz
 
-docker run -it --name landmark_inference --rm \
-            -v ${pwd}:/workdir/ \
-            -v snapshots_release:/snapshots/:ro \
-            -v OKOA:/data/:ro --ipc=host \
-            kneel_inference python -u inference_new_data.py \
-            --dataset_path ${HOME} \
-            --dataset_name OKOA \
-            --workdir ${HOME} \
-            --mean_std_path /snapshots/mean_std.npy\
-            --lc_snapshot_path /snapshots/lext-devbox_2019_07_14_16_04_41 \
-            --hc_snapshot_path /snapshots/lext-devbox_2019_07_14_16_04_41 \
-            --device cpu \
-            --refine True
+#docker run -it --name landmark_inference --rm \
+#            -v ${pwd}:/workdir/ \
+#            -v snapshots_release:/snapshots/:ro \
+#            -v OKOA:/data/:ro --ipc=host \
+#            kneel_inference python -u inference_new_data.py \
+#            --dataset_path ${HOME} \
+#            --dataset_name OKOA \
+#            --workdir ${HOME} \
+#            --mean_std_path /snapshots/mean_std.npy\
+#            --lc_snapshot_path /snapshots/lext-devbox_2019_07_14_16_04_41 \
+#            --hc_snapshot_path /snapshots/lext-devbox_2019_07_14_16_04_41 \
+#            --device cpu \
+#            --refine True
